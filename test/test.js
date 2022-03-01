a2a6e9550821fa2a3741187c2e6f71c23400a517
Pavan Belagatti <pavan@shippable.com>
2022-03-01 14:14:52 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,43 +1,15 @@
-// Requiring module
-const assert = require('assert');
+const expect = require('chai').expect;
 
-// We can group similar tests inside a describe block
-describe("Simple Calculations", () => {
-before(() => {
-	console.log( "This part executes once before all tests" );
-});
+describe("Testing with chai", () => {
+	it("Is returning 4 when adding 2 + 2", () => {
+	expect(2 + 2).to.equal(4);
+	});
 
-after(() => {
-	console.log( "This part executes once after all tests" );
-});
-	
-// We can add nested blocks for different tests
-describe( "Test1", () => {
-	beforeEach(() => {
-	console.log( "executes before every test" );
+	it("Is returning boolean value as true", () => {
+	expect(5 == 5).to.be.true;
 	});
 	
-	it("Is returning 5 when adding 2 + 3", () => {
-	assert.equal(2 + 3, 5);
-	});
-
-	it("Is returning 6 when multiplying 2 * 3", () => {
-	assert.equal(2*3, 6);
+	it("Are both the sentences matching", () => {
+	expect("This is working").to.equal('This is working');
 	});
 });
-
-describe("Test2", () => {
-	beforeEach(() => {
-	console.log( "executes before every test" );
-	});
-	
-	it("Is returning 4 when adding 2 + 3", () => {
-	assert.equal(2 + 3, 4);
-	});
-
-	it("Is returning 8 when multiplying 2 * 4", () => {
-	assert.equal(2*4, 8);
-	});
-});
-});
-
